aae306f1c9aa20a6e72d541a4193fe3f1a4b8e5c
Dhananjay <dhananjaybhardwaj.07@gmail.com>
2023-07-24 07:49:04 +0530

    contact

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {LiaTelegram} from 'react-icons/lia'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Dhananjay Bhardwaj</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/profile.php?id=100010085199556"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/dhananjaybhardwaj07/"><FaInstagram/></a>
+        <a href="https://t.me/dhananjay07b"><LiaTelegram/></a>
+      </div>
+
+    </footer>
   )
 }
 
