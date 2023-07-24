218752bc74bfa80a550f5408e432f9516295a274
Dhananjay <dhananjaybhardwaj.07@gmail.com>
2023-07-24 06:55:10 +0530

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0fjxus5', 'template_jmp7t8u', form.current, '9da2HNHJgx1qsJCBd')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset()
+  };
+  
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>dhananjaybhardwaj07@gmail.com</h5>
+            <a href="mailto:dhananjaybhardwaj07@gmail.com" target='_blank'>Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>dhananjaybhardwaj07</h5>
+            <a href="https://m.me/dhananjaybhardwaj07" target='_blank'>Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>~Dhananjay</h5>
+            <a href="https://wa.me/917023734388" target='_blank'>Send a Message</a>
+          </article>
+        </div>
+        
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message'></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
